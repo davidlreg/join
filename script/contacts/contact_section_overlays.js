4e42a741483bc29129d1bfb441f8dacff350d3e0
〽️david <146224919+davidlreg@users.noreply.github.com>
2025-01-22 11:22:27 +0100

    added logic to show edit conttact and floating contact section

--- script/contacts/contact_section_overlays.js
+++ script/contacts/contact_section_overlays.js
@@ -19,15 +19,79 @@ function showEditContactOverlay() {
                 <img src="/assets/img/default_contact_picture.png" />
               </div>
               <div class="contactForm">
-                <a class="closeButton"><span class="material-symbols-outlined"> close </span></a>
+
+                <div class="closeButton"> <a><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_274405_5666" style="mask-type: alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+              <rect width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_274405_5666)">
+              <path
+                d="M12 13.4L7.09999 18.3C6.91665 18.4834 6.68332 18.575 6.39999 18.575C6.11665 18.575 5.88332 18.4834 5.69999 18.3C5.51665 18.1167 5.42499 17.8834 5.42499 17.6C5.42499 17.3167 5.51665 17.0834 5.69999 16.9L10.6 12L5.69999 7.10005C5.51665 6.91672 5.42499 6.68338 5.42499 6.40005C5.42499 6.11672 5.51665 5.88338 5.69999 5.70005C5.88332 5.51672 6.11665 5.42505 6.39999 5.42505C6.68332 5.42505 6.91665 5.51672 7.09999 5.70005L12 10.6L16.9 5.70005C17.0833 5.51672 17.3167 5.42505 17.6 5.42505C17.8833 5.42505 18.1167 5.51672 18.3 5.70005C18.4833 5.88338 18.575 6.11672 18.575 6.40005C18.575 6.68338 18.4833 6.91672 18.3 7.10005L13.4 12L18.3 16.9C18.4833 17.0834 18.575 17.3167 18.575 17.6C18.575 17.8834 18.4833 18.1167 18.3 18.3C18.1167 18.4834 17.8833 18.575 17.6 18.575C17.3167 18.575 17.0833 18.4834 16.9 18.3L12 13.4Z"
+                fill="#2A3647"
+              />
+            </g></svg
+        ></a></div>
+
                 <div class="formSection">
-                  <input type="text" placeholder="Name" class="formInput" requiered/>
-                  <input type="email" placeholder="Email" class="formInput" requiered/>
-                  <input type="tel" placeholder="Phone" class="formInput" requiered/>
-                </div>
+          <div>
+            <input type="text" placeholder="Name" class="formInput" requiered />
+            <svg class="nameIconEditOverlay" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <mask id="mask0_274969_2511" style="mask-type: alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                <rect width="24" height="24" fill="#D9D9D9" />
+              </mask>
+              <g mask="url(#mask0_274969_2511)">
+                <path
+                  d="M12 12C10.9 12 9.95833 11.6083 9.175 10.825C8.39167 10.0417 8 9.1 8 8C8 6.9 8.39167 5.95833 9.175 5.175C9.95833 4.39167 10.9 4 12 4C13.1 4 14.0417 4.39167 14.825 5.175C15.6083 5.95833 16 6.9 16 8C16 9.1 15.6083 10.0417 14.825 10.825C14.0417 11.6083 13.1 12 12 12ZM18 20H6C5.45 20 4.97917 19.8042 4.5875 19.4125C4.19583 19.0208 4 18.55 4 18V17.2C4 16.6333 4.14583 16.1125 4.4375 15.6375C4.72917 15.1625 5.11667 14.8 5.6 14.55C6.63333 14.0333 7.68333 13.6458 8.75 13.3875C9.81667 13.1292 10.9 13 12 13C13.1 13 14.1833 13.1292 15.25 13.3875C16.3167 13.6458 17.3667 14.0333 18.4 14.55C18.8833 14.8 19.2708 15.1625 19.5625 15.6375C19.8542 16.1125 20 16.6333 20 17.2V18C20 18.55 19.8042 19.0208 19.4125 19.4125C19.0208 19.8042 18.55 20 18 20ZM6 18H18V17.2C18 17.0167 17.9542 16.85 17.8625 16.7C17.7708 16.55 17.65 16.4333 17.5 16.35C16.6 15.9 15.6917 15.5625 14.775 15.3375C13.8583 15.1125 12.9333 15 12 15C11.0667 15 10.1417 15.1125 9.225 15.3375C8.30833 15.5625 7.4 15.9 6.5 16.35C6.35 16.4333 6.22917 16.55 6.1375 16.7C6.04583 16.85 6 17.0167 6 17.2V18ZM12 10C12.55 10 13.0208 9.80417 13.4125 9.4125C13.8042 9.02083 14 8.55 14 8C14 7.45 13.8042 6.97917 13.4125 6.5875C13.0208 6.19583 12.55 6 12 6C11.45 6 10.9792 6.19583 10.5875 6.5875C10.1958 6.97917 10 7.45 10 8C10 8.55 10.1958 9.02083 10.5875 9.4125C10.9792 9.80417 11.45 10 12 10Z"
+                  fill="#A8A8A8"
+                />
+              </g>
+            </svg>
+          </div>
+
+          <div>
+            <input type="email" placeholder="Email" class="formInput" requiered /><svg class="mailIconEditOverlay" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <mask id="mask0_274969_2518" style="mask-type: alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                <rect width="24" height="24" fill="#D9D9D9" />
+              </mask>
+              <g mask="url(#mask0_274969_2518)">
+                <path
+                  d="M4 20C3.45 20 2.97917 19.8042 2.5875 19.4125C2.19583 19.0208 2 18.55 2 18V6C2 5.45 2.19583 4.97917 2.5875 4.5875C2.97917 4.19583 3.45 4 4 4H20C20.55 4 21.0208 4.19583 21.4125 4.5875C21.8042 4.97917 22 5.45 22 6V18C22 18.55 21.8042 19.0208 21.4125 19.4125C21.0208 19.8042 20.55 20 20 20H4ZM20 8L12.525 12.675C12.4417 12.725 12.3542 12.7625 12.2625 12.7875C12.1708 12.8125 12.0833 12.825 12 12.825C11.9167 12.825 11.8292 12.8125 11.7375 12.7875C11.6458 12.7625 11.5583 12.725 11.475 12.675L4 8V18H20V8ZM12 11L20 6H4L12 11ZM4 8.25V6.775V6.8V6.7875V8.25Z"
+                  fill="#A8A8A8"
+                />
+              </g>
+            </svg>
+          </div>
+
+          <div>
+            <input type="tel" placeholder="Phone" class="formInput" requiered /><svg class="phoneIconEditOverlay" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <mask id="mask0_274969_2525" style="mask-type: alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="25">
+                <rect y="0.5" width="24" height="24" fill="#D9D9D9" />
+              </mask>
+              <g mask="url(#mask0_274969_2525)">
+                <path
+                  d="M19.95 21.5C17.8667 21.5 15.8083 21.0458 13.775 20.1375C11.7417 19.2292 9.89167 17.9417 8.225 16.275C6.55833 14.6083 5.27083 12.7583 4.3625 10.725C3.45417 8.69167 3 6.63333 3 4.55C3 4.25 3.1 4 3.3 3.8C3.5 3.6 3.75 3.5 4.05 3.5H8.1C8.33333 3.5 8.54167 3.57917 8.725 3.7375C8.90833 3.89583 9.01667 4.08333 9.05 4.3L9.7 7.8C9.73333 8.06667 9.725 8.29167 9.675 8.475C9.625 8.65833 9.53333 8.81667 9.4 8.95L6.975 11.4C7.30833 12.0167 7.70417 12.6125 8.1625 13.1875C8.62083 13.7625 9.125 14.3167 9.675 14.85C10.1917 15.3667 10.7333 15.8458 11.3 16.2875C11.8667 16.7292 12.4667 17.1333 13.1 17.5L15.45 15.15C15.6 15 15.7958 14.8875 16.0375 14.8125C16.2792 14.7375 16.5167 14.7167 16.75 14.75L20.2 15.45C20.4333 15.5167 20.625 15.6375 20.775 15.8125C20.925 15.9875 21 16.1833 21 16.4V20.45C21 20.75 20.9 21 20.7 21.2C20.5 21.4 20.25 21.5 19.95 21.5ZM6.025 9.5L7.675 7.85L7.25 5.5H5.025C5.10833 6.18333 5.225 6.85833 5.375 7.525C5.525 8.19167 5.74167 8.85 6.025 9.5ZM14.975 18.45C15.625 18.7333 16.2875 18.9583 16.9625 19.125C17.6375 19.2917 18.3167 19.4 19 19.45V17.25L16.65 16.775L14.975 18.45Z"
+                  fill="#A8A8A8"
+                />
+              </g>
+            </svg>
+          </div>
+        </div>
                 <div class="formButtons">
-                  <button type="button" class="deleteButton">Delete</button>
-                  <button type="button" class="saveButton">Save</button>
+                   <button type="button" class="deleteButton"><p>Delete</p></button>
+                  <button type="button" class="saveContactButton">
+            <p>Save</p>
+            <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <mask id="mask0_274969_4267" style="mask-type: alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                <rect y="0.5" width="24" height="24" fill="#D9D9D9" />
+              </mask>
+              <g mask="url(#mask0_274969_4267)">
+                <path
+                  d="M9.55057 15.65L18.0256 7.175C18.2256 6.975 18.4631 6.875 18.7381 6.875C19.0131 6.875 19.2506 6.975 19.4506 7.175C19.6506 7.375 19.7506 7.6125 19.7506 7.8875C19.7506 8.1625 19.6506 8.4 19.4506 8.6L10.2506 17.8C10.0506 18 9.81724 18.1 9.55057 18.1C9.28391 18.1 9.05057 18 8.85057 17.8L4.55057 13.5C4.35057 13.3 4.25474 13.0625 4.26307 12.7875C4.27141 12.5125 4.37557 12.275 4.57557 12.075C4.77557 11.875 5.01307 11.775 5.28807 11.775C5.56307 11.775 5.80057 11.875 6.00057 12.075L9.55057 15.65Z"
+                  fill="white"
+                />
+              </g>
+            </svg>
+          </button>
                 </div>
               </div>
             </div>
@@ -115,6 +179,7 @@ function showAddContactOverlay() {
             </svg>
           </div>
         </div>
+
         <div class="formButtons">
           <button type="button" class="cancelButton">
             <p>Cancel</p>
@@ -162,7 +227,7 @@ function showFloatingContactOverlay() {
                 <div class="profileInformation">
                   <p><span id="surname">XXXXX</span> <span id="lastname">XXXXX</span></p>
                   <div class="symbols">
-                    <div class="editIcon">
+                    <div class="editIcon" onclick="openEditContact()">
                       <a>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 24 24">
                           <path
--- script/contacts/contacts.js
+++ script/contacts/contacts.js
@@ -1,3 +1,5 @@
+// Add new contact logic
+
 /**
  * Opens the overlay for adding a new contact, including setting content and event listeners.
  *
@@ -8,7 +10,7 @@ function openAddContactOverlay() {
   addOverlayBackground(overlayContainer);
   const overlay = overlayContainer.querySelector(".addContactOverlay");
   openOverlay(overlay);
-  addEventListenersToOverlay(overlay);
+  addEventListenersToAddContactOverlay(overlay);
 }
 
 /**
@@ -36,7 +38,7 @@ function openOverlay(overlay) {
  *
  * @param {Element} overlay The overlay element to add event listeners to.
  */
-function addEventListenersToOverlay(overlay) {
+function addEventListenersToAddContactOverlay(overlay) {
   const closeButton = overlay.querySelector(".closeButton");
   const cancelButton = overlay.querySelector(".cancelButton");
   closeButton.addEventListener("click", closeAddContactOverlay);
@@ -72,4 +74,66 @@ function removeOverlayContent() {
     overlayContainer.innerHTML = "";
     overlayContainer.classList.remove("overlayBackground");
   }, 300);
+}
+
+// FLoating contact logic
+
+/**
+ * Inserts the floating contact template into the div provided for this purpose
+ *
+ */
+function openContact() {
+  const floatingConntactContainer = document.getElementById("floatingContactContainer");
+  floatingConntactContainer.innerHTML = showFloatingContactOverlay();
+  const overlay = floatingContactContainer.querySelector(".profileHeadSection");
+  openFloatingContactOverlay(overlay);
+}
+
+/**
+ * Opens the provided overlay with a smooth transition effect.
+ *
+ * @param {Element} overlay The overlay element to be opened.
+ */
+function openFloatingContactOverlay(overlay) {
+  overlay.style.transition = "transform 0.3s ease-in-out";
+  overlay.style.transform = "translateX(100%)";
+  setTimeout(() => (overlay.style.transform = "translateX(0)"), 100);
+}
+
+// Edit contact logic
+
+/**
+ * Opens the overlay for editing a existing contact, including setting content and event listeners.
+ *
+ */
+function openEditContact() {
+  const overlayContainer = document.getElementById("overlayContainer");
+  overlayContainer.innerHTML = showEditContactOverlay();
+
+  addOverlayBackground(overlayContainer);
+  const overlay = overlayContainer.querySelector(".editContactOverlay");
+  openOverlay(overlay);
+  addEventListenersToEditContactOverlay(overlay);
+}
+
+/**
+ * Adds event listeners for closing the overlay to the close and cancel buttons.
+ *
+ * @param {Element} overlay The overlay element to add event listeners to.
+ */
+function addEventListenersToEditContactOverlay(overlay) {
+  const deleteButton = overlay.querySelector(".deleteButton");
+  const closeButton = overlay.querySelector(".closeButton");
+  deleteButton.addEventListener("click", closeEditContactOverlay);
+  closeButton.addEventListener("click", closeEditContactOverlay);
+}
+
+/**
+ * Closes the add contact overlay and removes its content.
+ *
+ */
+function closeEditContactOverlay() {
+  const overlay = document.querySelector(".editContactOverlay");
+  closeOverlay(overlay);
+  removeOverlayContent();
 }

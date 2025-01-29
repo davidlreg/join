47969a75401252c0c11896d6a73814ae69ab5f65
Stefan Redl <bernhardlaus@gmx.at>
2025-01-29 22:12:25 +0100

    design Task for board; add boardOverlay when a task was clicked;

--- script/board/board.js
+++ script/board/board.js
@@ -4,6 +4,8 @@ function init() {
 
 function getBoardElements() {
   return {
+    boardSectionsSubContainer: document.querySelectorAll('.boardSectionsSubContainer'),
+    boardTasks: document.querySelector('.boardTasks'),
     tasksToDo: document.getElementById('buttonNoTasksToDo'),
     tasksInProgess: document.getElementById('buttonNoTasksInProgess'),
     tasksAwaiting: document.getElementById('buttonNoTasksAwaiting'),
@@ -11,23 +13,28 @@ function getBoardElements() {
   };
 }
 
-function setTasksToDo() {
-  const { tasksToDo } = getBoardElements();
-  
+function addBoardOverlay() {
+  let boardOverlay = document.getElementById('addBoardOverlay');
+  let dynamicContent = document.getElementById('overlayBoardContent');
+
+  let addBoardHtml = templateBoardOverlay();
+
+  dynamicContent.innerHTML = addBoardHtml;
+
+  boardOverlay.classList.remove('hideOverlay');
+}
+
+function closeBoardOverlay(){
+  let boardOverlay = document.getElementById('addBoardOverlay');
+  boardOverlay.classList.add('hideOverlay');
 }
 
 
-function showTask() {
-  const { tasksToDo, tasksInProgess, tasksAwaiting, tasksDone} = getBoardElements();
+// function updateSubtaskProgress(completed, total) {
+//   const progressBar = document.querySelector('.subtask-progress-bar');
+//   const percentage = (completed / total) * 100;
+//   progressBar.style.width = percentage + '%';
+// }
 
-}
-
-function updateSubtaskProgress(completed, total) {
-  const progressBar = document.querySelector('.subtask-progress-bar');
-  const percentage = (completed / total) * 100;
-  progressBar.style.width = percentage + '%';
-}
-
-// Testaufruf mit 2 von 5 erledigten Subtasks
-updateSubtaskProgress(2, 5);
-
+// // Testaufruf mit 2 von 5 erledigten Subtasks
+// updateSubtaskProgress(2, 5);

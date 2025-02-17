da38917dc3f4a3004e1688ab5453059e9388fa58
〽️david <146224919+davidlreg@users.noreply.github.com>
2025-02-17 21:32:38 +0100

    styling changes for edit and add contact Templates

--- script/contact/contactTemplates.js
+++ script/contact/contactTemplates.js
@@ -469,7 +469,7 @@ function showEditContactOverlayMobile(initials, color) {
 
   <div class="whiteBackgroundEdit">
     <div class="formInputMobile">
-      <input type="text" placeholder="Name" value="${currentlyViewedUser.name}" id="contactName"/>
+      <input type="text" placeholder="Name" class="formInput" value="${currentlyViewedUser.name}" id="contactName"/>
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="nameIconMobileEdit">
         <mask id="mask0_274969_2511" style="mask-type: alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
           <rect width="24" height="24" fill="#D9D9D9" />

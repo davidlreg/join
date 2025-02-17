b0f450c09656a80205c68685ee20991cdeb67bc9
Stefan Redl <bernhardlaus@gmx.at>
2025-02-17 16:38:32 +0100

    add hovereffect to edit and delete.

--- script/board/boardTemplates.js
+++ script/board/boardTemplates.js
@@ -131,7 +131,7 @@ function templateBoardOverlay(task, taskId) {
               Delete
             </button>
             <div class="overlaySeparator">|</div>
-            <button class="boardOverlayActionButtonsEdit"  onclick="editTask('${taskId}')" data-task-id="${taskId}>
+            <button class="boardOverlayActionButtonsEdit" onclick="editTask('${taskId}')">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <mask id="mask0_278437_2498" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
                 <rect width="24" height="24" fill="#D9D9D9"/>
